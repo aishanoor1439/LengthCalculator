7f0d46db3e07be79d8eabaebd771af1d090441dd
aishanoor1439 <aisha.noor1439@gmail.com>
2025-03-11 09:55:16 +0100

    Final Commit
    added project files

---
+++ LengthCalculator/Length.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Length
+    {
+
+//Attributes
+        int feet, inch;
+
+//Methods
+        public Length()
+        {
+            this.feet = 0;
+            this.inch = 0;
+        }
+        public Length(int feet, int inch)
+        {
+            this.feet = feet;
+            this.inch = inch;
+        }
+        public string getLength()
+        {
+            return string.Format("Length : {0}' {1} \"", feet, inch);
+        }
+
+//Operator overloading
+        public static Length operator +(Length l1, Length l2)
+        {
+            Length l3 = new Length();
+            l3.feet = l1.feet + l2.feet;
+            l3.inch = l1.inch + l2.inch;
+            if (l3.inch >= 12)
+            {
+                l3.feet++;
+                l3.inch -= 12;
+            }
+            return l3;
+        }
+    }
+}
---
+++ LengthCalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Length len1 = new Length(2, 8);
+            Length len2 = new Length(4, 5);
+            Length len3 = len1 + len2;
+            Console.WriteLine(len1.getLength());
+            Console.WriteLine(len2.getLength());
+            Console.WriteLine(len3.getLength());
+            Console.ReadKey();
+        }
+    }
+}
